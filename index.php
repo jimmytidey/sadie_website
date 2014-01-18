16770c151658a9eed6e38939cc8be2674bb98beb
OpenShift guest <jimmytidey@gmail.com>
2014-01-18 17:46:09 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php include("header.php")  ?>
+
+
+
+<div class='container'> 
+    
+    
+    <div class='row'>
+        <div class='col-md-12'> 
+            <h1 class='main_header'>
+                I'm Sadie Edginton. <a href=''>My practise</a> uses live situations to explore social structures. I work on <a href=''>community projects</a> and <a href=''>organise events</a>.   
+                
+            </h1>
+        </div>
+    </div>
+    
+    <div class="header_watermark"> 
+        <p>'Weaving Bankers' Outside Departure Foundation, Monument, London. Part of my investigation of using human bodies as a material.
+        <em></p>
+    </div>
+    
+    <div class='row'>
+        <img class='col-md-12' src='images/weaving_bankers_1.jpg' />
+    </div>
+    
+    
+    
+</div>
+
+
+
+
+
+<?php include("footer.php")  ?>
